fa383a5aaf200b55cda998a05072bac8dab11ec3
Ahmad <ak12hastam@gmail.com>
2020-02-27 21:17:34 +0330

    Add custom user to admin

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,41 @@
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+class AdminSiteTest(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_super_user(
+            email='admin@example.com',
+            password='adminpass'
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='regular@example.com',
+            password='regularpass',
+            name='regular user name'
+        )
+
+    def test_users_listed(self):
+        """Test that users are listed on user page"""
+        url = reverse('admin:core_user_changelist')
+        response = self.client.get(url)
+
+        self.assertContains(response, self.user.email)
+        self.assertContains(response, self.user.name)
+
+    def test_user_change_page(self):
+        """Test if the user edit page works"""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_user_create_page(self):
+        """Test if the user create page works"""
+        url = reverse('admin:core_user_add')
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
